3c6973013220e685567247bd72d8bf5e31627b7f
monster <monster@hamsterbase.com>
2022-09-21 00:07:36 +0800

    feat: init antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
+import { Button, InputNumber } from 'antd';
+import 'antd/dist/antd.css';
 import { useState } from 'react';
 import styles from './app.module.less';
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
+  const [port, setPort] = useState<number | null>(4000);
 
   return (
     <div className={styles.app}>
+      <InputNumber
+        placeholder="4000"
+        value={port}
+        onChange={(e) => setPort(e)}
+      />
       <div>{count}</div>
-      <button
+      <Button
         onClick={() => {
           setCount((e) => e + 1);
         }}
       >
         Add
-      </button>
+      </Button>
     </div>
   );
 };
